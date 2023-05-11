2b4d62b4d448d993fc95370c6d31c93422d5a7e2
Henrique Oliveira <henriqu3oliveir@gmail.com>
2023-05-11 14:30:23 -0300

    services

--- src/app/controle-editora.service.ts
+++ src/app/controle-editora.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Editora } from './editora';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ControleEditoraService {
 
-  constructor() { }
+export class ControleEditoraService {
+  editoras: Array<Editora>
+
+  //funcao recebe codeditora do tipo number como parametro e retorna o nome da editora em string
+
+  getNomeEditora(codEditora: number): string {
+    for (let editora of this.editoras) {
+      if (editora.codEditora == codEditora) {
+        return editora.nome;
+      }
+    }
+    return '';
+  }
+
+  //funcao retorna um array de editoras
+
+  getEditoras(): Array<Editora> {
+    return this.editoras;
+  }
+
+  constructor() {
+    this.editoras = new Array<Editora>();
+    this.editoras.push(new Editora(1, 'Editora 1'));
+    this.editoras.push(new Editora(2, 'Editora 2'));
+    this.editoras.push(new Editora(3, 'Editora 3'));
+  }
 }
--- src/app/controle-livros.service.ts
+++ src/app/controle-livros.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
-
+import { Livro } from './livro';
 @Injectable({
   providedIn: 'root'
 })
 export class ControleLivrosService {
+  
+  livros: Array<Livro>
 
-  constructor() { }
+  obterLivros(): Array<Livro> {
+    return this.livros;
+  }
+
+  incluir(livro: Livro): void {
+    this.livros.push(livro);
+  }
+
+  excluir(codigo: number): void {
+    for (let i = 0; i < this.livros.length; i++) {
+      if (this.livros[i].codigo == codigo) {
+        this.livros.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+
+  constructor() { 
+    this.livros = new Array<Livro>();
+
+    this.livros.push(new Livro(1, 1, 'Livro 1', 'Resumo do livro 1', ['Autor 1', 'Autor 2']))
+    this.livros.push(new Livro(2, 2, 'Livro 2', 'Resumo do livro 2', ['Autor 3', 'Autor 4']))
+    this.livros.push(new Livro(3, 3, 'Livro 3', 'Resumo do livro 3', ['Autor 5', 'Autor 6']))
+
+  }
 }
